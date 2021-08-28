e9a7e9ba1e66b739cfe865fc201b7bdd820388f2
KendallMM <kendallmarinmunoz@gmail.com>
2021-08-28 11:16:18 -0600

    Ventana de prueba terminada, añadí entradas y salidas de texto y una choice box

--- src/Chat/Controller.java
+++ src/Chat/Controller.java
@@ -1,14 +1,41 @@
 package Chat;
 
-import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 
-public class Controller {
-    @FXML
-    private Button btnOK;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    public void handleBtnOK(ActionEvent actionEvent) {
-        System.out.print("Hello Word JavaFX!");
+
+
+public class Controller implements Initializable {
+
+    @FXML public TextField tfChat;
+    @FXML public Button btnOK;
+    @FXML public Slider charSlider;
+    @FXML private TextField tfNombre;
+    @FXML private ChoiceBox<String> chGen;
+
+    @FXML private TextField tfMensaje;
+
+
+
+    @FXML public void accionAceptar() {
+        String nombre = tfNombre.getText() + ": ";
+        String mensaje = tfMensaje.getText();
+        String gen = chGen.getValue();
+        tfChat.setText(nombre+mensaje+gen);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        tfNombre.setText("---");
+        chGen.getItems().add("Masculino");
+        chGen.getItems().add("Femenino");
     }
 }
